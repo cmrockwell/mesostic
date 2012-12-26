6c9803421845a3a58773b7e2fb57d7b47bfcf789
Chris Rockwell <cmrockwell@yahoo.com>
2012-12-26 16:12:57 -0500

    makes array of word from the seed text

--- spec/MesoSpec.js
+++ spec/MesoSpec.js
@@ -3,26 +3,35 @@
  */
 
 describe('Mesostic Generator',function(){
-
-
+	
 	beforeEach(function () {  	
+
 		meso = new Mesostic();
-	});
-
-	it('has a spine word',function(){	 
+		//meso.init(); 
 		meso.setSpine("SPINE");
-		expect(meso.getSpine()).toEqual("SPINE");
-	});
-
-
-	it('has seed text', function() {
 		meso.setSeed("A mesostic is a poem or other typography such that a vertical phrase"+
 		"intersects lines of horizontal text. It is similar to an acrostic, but with the vertical"+
 		" phrase intersecting the middle of the line, as opposed to beginning each new line.")
-		expect(meso.getSeed()).toEqual("A mesostic is a poem or other typography such that a vertical phrase"+
-		"intersects lines of horizontal text. It is similar to an acrostic, but with the vertical"+
-		" phrase intersecting the middle of the line, as opposed to beginning each new line.");
 	});
-  
+
+	it('has a spine word',function(){	 		
+		expect(meso.getSpine()).toEqual("SPINE");
+	});
+
+	it('has seed text', function() {
+		expect(meso.getSeed(1)).toEqual("mesostic");
+	});
+	
+	xit('finds the next word that contains the current letter in the spine word',function(){	 		
+		expect(meso.getWord(0)).toEqual("mesostic");
+	});
+
+	xit('the found word does not contain next spine letter after the current letter in found word',function(){	 		
+		expect(meso.getWord(1)).toEqual("mesostic");
+	});  
+	
+	xit('the found word does not repeat current spine letter (only for 100% mesostic)',function(){	 		
+		expect(meso.getWord(2)).toEqual("mesostic");
+	});  	
   
 });
--- src/Mesostic.js
+++ src/Mesostic.js
@@ -5,6 +5,11 @@ var Mesostic = function() {
 	var spineWord="";
 	var seedText="";
 	var poem="";
+	var textArr=[];
+};
+Mesostic.prototype.init = function() {
+	//var pattern = /[a-z-']+/i; // matches words with no digits
+    //textArr = seedText.match(pattern);
 };
 
 Mesostic.prototype.setSpine = function(sp) {
@@ -15,11 +20,16 @@ Mesostic.prototype.getSpine = function() {
 };
 Mesostic.prototype.setSeed = function(seed) {
     seedText = seed;
+    var pattern = /[a-z-']+/gi; // matches words with no digits
+    textArr = seedText.match(pattern);
 };
-Mesostic.prototype.getSeed = function() {
-    return seedText;
+Mesostic.prototype.getSeed = function(ind) {
+    return textArr[ind];
 };
 
+Mesostic.prototype.getWord = function(index){
+	var letter = spineWord.substring(index, index+1);
+};
 
 
 window.Mesostic = Mesostic;
